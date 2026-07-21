bda0c1064cacebd14a88403abd3a896043194879
Tran Bao Minh <tranbaominh205@gmail.com>
2026-07-21 16:13:48 +0700

    Initial commit

---
+++ src/main/java/com/tbm/idenity_service/IdenityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tbm.idenity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdenityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdenityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tbm/idenity_service/IdenityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbm.idenity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdenityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
