9d34c5d39a67b252658ba42757edd787ba44f820
Tran Bao Minh <tranbaominh205@gmail.com>
2026-07-29 13:16:36 +0700

    feat(exception): dynamic HTTP status codes and enhanced error responses

--- src/main/java/com/tbm/idenity_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tbm/idenity_service/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.tbm.idenity_service.exception;
 import com.tbm.idenity_service.dto.request.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,8 +27,18 @@ public class GlobalExceptionHandler {
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(errorCode.getCode());
         apiResponse.setMessage(errorCode.getMessage());
-        return ResponseEntity.badRequest().body(apiResponse);
+        return ResponseEntity.status(errorCode.getHttpStatusCode()).body(apiResponse);
     }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException accessDeniedException){
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+
+        return ResponseEntity.status(errorCode.getHttpStatusCode()).body(ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage()).build());
+    }
+
 
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
