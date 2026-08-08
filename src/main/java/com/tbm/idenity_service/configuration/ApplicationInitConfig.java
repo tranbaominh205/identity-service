a8bca6eef8f567db211edb0e2d4f1c74a8ceefc6
Tran Bao Minh <tranbaominh205@gmail.com>
2026-08-08 14:23:27 +0700

    test(user): apply unit test isolation and best practices

--- src/main/java/com/tbm/idenity_service/configuration/ApplicationInitConfig.java
+++ src/main/java/com/tbm/idenity_service/configuration/ApplicationInitConfig.java
@@ -9,6 +9,8 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,6 +26,10 @@ public class ApplicationInitConfig {
     PasswordEncoder passwordEncoder;
 
     @Bean
+    @ConditionalOnProperty(prefix = "spring",
+            value = "datasource.driverClassname",
+            havingValue = "com.mysql.cj.jdbc.Driver"
+    )
     ApplicationRunner applicationRunner(UserRepository userRepository){
         return args -> {
             if (userRepository.findByUsername("admin").isEmpty())
