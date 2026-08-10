5fdb1d5882a9e2d86782c317d5fd0ffdbd17e225
Tran Bao Minh <tranbaominh205@gmail.com>
2026-08-10 18:07:39 +0700

    feat(jpa): handle unique field validation under concurrent requests

--- src/main/java/com/tbm/identity_service/service/UserService.java
+++ src/main/java/com/tbm/identity_service/service/UserService.java
@@ -3,6 +3,7 @@ package com.tbm.identity_service.service;
 import java.util.HashSet;
 import java.util.List;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -36,13 +37,18 @@ public class UserService {
     RoleRepository roleRepository;
 
     public UserResponse createUser(UserCreationRequest request) {
-        if (userRepository.existsByUsername(request.getUsername())) throw new AppException(ErrorCode.USER_EXISTED);
         User user = userMapper.toUser(request);
         user.setPassword(passwordEncoder.encode(request.getPassword()));
 
         HashSet<String> roles = new HashSet<>();
         roles.add(Role.USER.name());
         // user.setRoles(roles);
+        try {
+            user = userRepository.save(user);
+        }catch (DataIntegrityViolationException exception){
+            throw new AppException(ErrorCode.USER_EXISTED);
+        }
+
 
         return userMapper.toUserResponse(userRepository.save(user));
     }
